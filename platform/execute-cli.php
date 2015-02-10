0cfa8bf4aece69d77a8e76dd8e7d571bf062e47d
Andrew Cuthbert <acuthbert@gcdtech.com>
2015-02-10 09:57:45 +0000

    Fixed cli execution script - working path was wrong

--- platform/execute-cli.php
+++ platform/execute-cli.php
@@ -21,7 +21,7 @@
  */
 
 // Change the working directory to the top level project folder.
-chdir(__DIR__ . "/../../../../../");
+chdir(__DIR__ . "/../../../../");
 
 // Initiate our bootstrap script to boot all libraries required.
 require_once __DIR__ . "/boot.php";
