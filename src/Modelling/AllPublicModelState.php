09f68020fe9b5b20c9681808e32554502fba268f
Andrew Cuthbert <acuthbert@gcdtech.com>
2015-06-16 23:20:00 +0100

    Added AllPublicModelState

---
+++ src/Modelling/AllPublicModelState.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ *	Copyright 2015 RhubarbPHP
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+namespace Rhubarb\Crown\Modelling;
+
+/**
+ * A form of ModelState that makes all of it's properties public for serialization.
+ *
+ * This should only be used when creating data structure models where all model properties
+ * can be considered public.
+ */
+class AllPublicModelState extends ModelState
+{
+    protected function getPublicPropertyList()
+    {
+        return array_keys( $this->modelData );
+    }
+}
