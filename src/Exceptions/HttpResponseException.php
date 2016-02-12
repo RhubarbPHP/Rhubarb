9b100a22171f53c0802909082f1dcc4b0b13993e
Michael Miscampbell <mmiscampbell@gcdtech.com>
2016-02-12 16:35:28 +0000

    Adding property to store the HttpResponse that caused the exception

--- src/Exceptions/HttpResponseException.php
+++ src/Exceptions/HttpResponseException.php
@@ -2,7 +2,16 @@
 
 namespace Rhubarb\Crown\Exceptions;
 
+use Rhubarb\Crown\Http\HttpResponse;
+
 class HttpResponseException extends RhubarbException
 {
+    public $response;
+
+    public function __construct($privateMessage = "", \Exception $previous = null, HttpResponse $response = null)
+    {
+        parent::__construct($privateMessage, $previous);
+        $this->response = $response;
+    }
 
 }
