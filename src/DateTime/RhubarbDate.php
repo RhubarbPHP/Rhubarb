d1543956b39d811c466b7550527e4d0267dd6be6
NiallSmyth <nsmyth@gcdtech.com>
2016-03-16 11:42:35 +0000

    Converting timezones on RhubarbDates to ensure they get the correct day even if their initial timezone is different

--- src/DateTime/RhubarbDate.php
+++ src/DateTime/RhubarbDate.php
@@ -26,6 +26,10 @@ class RhubarbDate extends RhubarbDateTime
     {
         parent::__construct($dateValue, $timezone);
 
+        // Set the timezone to the system default in case it was created in a different timezone, so
+        // we can treat it as timezone agnostic
+        $this->setTimezone(new DateTimeZone(date_default_timezone_get()));
+
         // Dates must be timezone agnostic as they don't store a time. We need to 'zero' out the time portion
         parent::setTime(0, 0, 0);
     }
