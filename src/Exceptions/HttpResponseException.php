8902541ad9dc8ef49ebd1be93da71a58185dcfc7
Michael Miscampbell <mmiscampbell@gcdtech.com>
2016-04-21 22:17:54 +0100

    Adding HttpRequest Object to HttpResponseException

--- src/Exceptions/HttpResponseException.php
+++ src/Exceptions/HttpResponseException.php
@@ -2,13 +2,14 @@
 
 namespace Rhubarb\Crown\Exceptions;
 
+use Rhubarb\Crown\Http\HttpRequest;
 use Rhubarb\Crown\Http\HttpResponse;
 
 class HttpResponseException extends RhubarbException
 {
     public $response;
 
-    public function __construct($privateMessage = "", \Exception $previous = null, HttpResponse $response = null)
+    public function __construct($privateMessage = "", \Exception $previous = null, HttpResponse $response = null, HttpRequest $request = null)
     {
         parent::__construct($privateMessage, $previous);
         $this->response = $response;
