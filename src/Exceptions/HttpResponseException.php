3fb0b41f9eb40be08960c1c6be23c38400262341
Michael Miscampbell <mmiscampbell@gcdtech.com>
2016-04-21 22:22:24 +0100

    Adding public accessor Http Request object

--- src/Exceptions/HttpResponseException.php
+++ src/Exceptions/HttpResponseException.php
@@ -8,10 +8,12 @@ use Rhubarb\Crown\Http\HttpResponse;
 class HttpResponseException extends RhubarbException
 {
     public $response;
+    public $request;
 
     public function __construct($privateMessage = "", \Exception $previous = null, HttpResponse $response = null, HttpRequest $request = null)
     {
         parent::__construct($privateMessage, $previous);
         $this->response = $response;
+        $this->request = $request;
     }
 }
