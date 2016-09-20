e2201a8a5a3913ace54b3b87aa02a5152e7464d2
Sam Thompson <sthompson@gcdtech.com>
2016-09-20 15:39:24 +0100

    cannot read from php://input for multipart requests
    refactored request to populate post and file data instead

--- src/Request/MultiPartFormDataRequest.php
+++ src/Request/MultiPartFormDataRequest.php
@@ -18,6 +18,7 @@
 namespace Rhubarb\Crown\Request;
 
 use Rhubarb\Crown\Mime\MimeDocument;
+use Rhubarb\Crown\Mime\MimePart;
 
 class MultiPartFormDataRequest extends WebRequest
 {
@@ -25,34 +26,71 @@ class MultiPartFormDataRequest extends WebRequest
     /** @var null|string */
     private $rawRequest = null;
 
+    public function getPayload()
+    {
+        // Support multipart file PUT operations
+        if (isset($this->serverData['REQUEST_METHOD']) && $this->serverData['REQUEST_METHOD'] === 'PUT') {
+            $this->parsePut();
+        }
+
+        $requestBody = array_merge($this->filesData, $this->postData);
+        return $requestBody;
+    }
+
     /**
      * @return string
      */
-    public function getRawRequest()
+    public function parsePut()
     {
         // return raw request if set - this is for testing purposes
         if ($this->rawRequest !== null) {
-            return $this->rawRequest;
+            $rawRequest = $this->rawRequest;
+        } else {
+            /* PUT data only comes in on the stdin stream, so I'm reading for that for POST/PUT/FILES*/
+            $requestStream = fopen('php://input', 'r');
+
+            $requestBody = '';
+            // read
+            while ($chunk = fread($requestStream, 1024)) {
+                $requestBody .= $chunk;
+            }
+            $requestBody = ltrim($requestBody);
+
+            fclose($requestStream);
+
+            $headers = '';
+            foreach ($this->headerData as $header => $value) {
+                $headers .= "{$header}: {$value}\n";
+            }
+
+            $rawRequest = $headers . "\n" . $requestBody;
         }
 
-        /* PUT data only comes in on the stdin stream, so I'm reading for that for POST/PUT/FILES*/
-        $requestStream = fopen('php://input', 'r');
-
-        $requestBody = '';
-        // read
-        while ($chunk = fread($requestStream, 1024)) {
-            $requestBody .= $chunk;
+        $document = MimeDocument::fromString($rawRequest);
+        foreach ($document->getParts() as $part) {
+            /** @var MimePart $part */
+            foreach ($part->getHeaders() as $header => $value) {
+                if (strtolower($header) === 'content-disposition') {
+                    $partName = preg_replace('/.*? name="(.+?)".*/', '$1', $value);
+                    if (stripos($value, ' filename=') !== false) {
+                        $fileName = preg_replace('/.*? filename="(.+?)"/', '$1', $value);
+                        $tempFile = tempnam(sys_get_temp_dir(), 'put-file-');
+                        $fileContents = $part->getTransformedBody();
+                        file_put_contents($tempFile, $fileContents);
+                        $this->filesData[$partName] = [
+                            'name' => $fileName,
+                            'type' => @mime_content_type($fileName),
+                            'tmp_name' => $tempFile,
+                            'error' => UPLOAD_ERR_OK,
+                            'size' => strlen($fileContents),
+                        ];
+                    } else {
+                        //this does not look like a file, add it to the post data
+                        $this->postData[$partName] = $part->getTransformedBody();
+                    }
+                }
+            }
         }
-        $requestBody = ltrim($requestBody);
-
-        fclose($requestStream);
-
-        $headers = '';
-        foreach ($this->headerData as $header => $value) {
-            $headers .= "{$header}: {$value}\n";
-        }
-
-        return $headers . "\n" . $requestBody;
     }
 
     /**
@@ -61,13 +99,5 @@ class MultiPartFormDataRequest extends WebRequest
     public function setRawRequest($rawRequest)
     {
         $this->rawRequest = $rawRequest;
-    }
-
-    /**
-     * @return MimeDocument
-     */
-    public function getPayload()
-    {
-        return MimeDocument::fromString($this->getRawRequest());
     }
 }
