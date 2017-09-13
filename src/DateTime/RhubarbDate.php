2914edb0a16a5e9a61e0471b3b152287c4ccefa1
NiallSmyth <nsmyth@gcdtech.com>
2017-09-13 12:07:52 +0100

    RhubarbDate class compatible with PHP >= 7.1 (setTime method gained an extra parameter)

--- src/DateTime/RhubarbDate.php
+++ src/DateTime/RhubarbDate.php
@@ -20,7 +20,7 @@ namespace Rhubarb\Crown\DateTime;
 
 use DateTimeZone;
 
-class RhubarbDate extends RhubarbDateTime
+abstract class AbstractRhubarbDate extends RhubarbDateTime
 {
     public function __construct($dateValue = '', DateTimeZone $timezone = null)
     {
@@ -33,14 +33,27 @@ class RhubarbDate extends RhubarbDateTime
         parent::__construct($this->format('Y-m-d 00:00:00'), $timezone);
     }
 
-    public function setTime($hour, $minute, $second = 0)
-    {
-        return parent::setTime(0, 0, 0);
-    }
-
     public static function createFromFormat($format, $time, $timezone = null)
     {
         $date = parent::createFromFormat($format, $time, $timezone);
         return new RhubarbDate($date);
+    }
+}
+
+if (PHP_VERSION_ID >= 70100) {
+    class RhubarbDate extends AbstractRhubarbDate
+    {
+        public function setTime($hour, $minute, $second = 0, $microseconds = 0)
+        {
+            return parent::setTime(0, 0, 0, 0);
+        }
+    }
+} else {
+    class RhubarbDate extends AbstractRhubarbDate
+    {
+        public function setTime($hour, $minute, $second = 0)
+        {
+            return parent::setTime(0, 0, 0);
+        }
     }
 }
