66154dc78bf97eae0eaf23076477fc99734d794e
catherineGCD <catherine@gcdtech.com>
2017-11-15 23:50:59 +0000

    SMS Sendable (#54)
    * sms sendable support
    
    * updating changelog
    
    * making SMS not abstract

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Change log
 
+### 1.5.0
+
+* Added: SMS Sendable support
+
 ### 1.4.1
 
 * Changed: PhpSessionProvider now sets HttpOnly to true
---
+++ src/Sendables/SMS/SMSProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Copyright (c) 2016 RhubarbPHP.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Rhubarb\Crown\Sendables\SMS;
+
+use Rhubarb\Crown\DependencyInjection\ProviderInterface;
+use Rhubarb\Crown\DependencyInjection\ProviderTrait;
+use Rhubarb\Crown\Sendables\SendableProvider;
+
+/**
+ * Performs transmission of SMS messages to recipients.
+ *
+ * There is a default SMS provider which can be changed by calling the static setDefaultSMSProviderClassName()
+ * function. Similarly to get the default SMS provider for your app simply call
+ * SMSProvider::getDefaultSMSProvider()
+ *
+ * @see SMS
+ */
+abstract class SMSProvider extends SendableProvider implements ProviderInterface
+{
+    use ProviderTrait;
+}
